9d9879d295704aea75e4a19b88ccdee8b891b16e
NirantK <nirant.bits@gmail.com>
2025-05-18 11:46:48 +0530

    enhance main function: add timestamp and UUID for unique namespace in write_to_turbopuffer

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
+import uuid
+from datetime import datetime
+
+from src.ingest import fastembedding, write_to_turbopuffer
 from src.prepare import prepare
 
 
 def main():
+    now = datetime.now()
+    date = now.strftime("%Y-%m-%d")
+    hour = now.hour
     prepare(
         pdf_path="pdfs",
         markdown_path="data/markdown",
         images_path="data/images",
         chunks_path="data/chunks",
+        document_info_path="data/ground_truth/financebench_document_information.jsonl",
+    )
+    write_to_turbopuffer(
+        embedding_function=fastembedding,
+        annotated_chunks_filepath="data/processed/annotated_chunks.jsonl",
+        namespace=f"nirantk-fastembedding-{date}-{hour}-{uuid.uuid4()}",
     )
 
 
