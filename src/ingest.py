41ceb70dc22baba485491b44fac852d4aecae35d
NirantK <nirant.bits@gmail.com>
2025-05-18 12:19:21 +0530

    enhance dependencies and file handling: add joblib for vector storage and update .gitignore for new data paths

--- src/ingest.py
+++ src/ingest.py
@@ -1,13 +1,16 @@
 import os
 from itertools import batched
+from pathlib import Path
 from typing import Callable, List
 
+import joblib
 import pandas as pd
 import turbopuffer as tpuf
 from dotenv import load_dotenv
 from fastembed import TextEmbedding
 from loguru import logger
 from openai import OpenAI
+from tqdm import tqdm
 
 load_dotenv()
 client = OpenAI()
@@ -26,10 +29,34 @@ def openai_embedding(texts: List[str], model: str = "text-embedding-3-large"):
 
 
 def fastembedding(
-    texts: List[str], model: str = "mixedbread-ai/mxbai-embed-large-v1"
+    texts: List[str], model: str = "snowflake/snowflake-arctic-embed-xs"
 ) -> List[List[float]]:
     fst = TextEmbedding(model=model)
-    return list(fst.embed(texts))
+    vectors = []
+    batch_size = 512
+    for index, batch in tqdm(
+        enumerate(batched(texts, batch_size)),
+        desc=f"Embedding texts with {model}",
+        total=len(texts) // batch_size,
+        unit="batch",
+    ):
+        vectors.extend(list(fst.embed(batch)))
+        # Make sure the directory exists
+        vectors_path = Path("data/vectors")
+        vectors_path.mkdir(parents=True, exist_ok=True)
+        joblib.dump(vectors, vectors_path / f"{index}.pkl")
+    return vectors
+
+
+def get_vectors(
+    annotated_chunks_filepath: str,
+    embedding_function: Callable[[List[str]], List[List[float]]],
+) -> tuple[pd.DataFrame, List[List[float]]]:
+    annotated_chunks = pd.read_json(annotated_chunks_filepath, lines=True)
+    vectors = embedding_function(annotated_chunks["text"].tolist())
+    logger.info(f"Got {len(vectors)} vectors")
+
+    return annotated_chunks, vectors
 
 
 def write_to_turbopuffer(
@@ -37,16 +64,15 @@ def write_to_turbopuffer(
     annotated_chunks_filepath: str,
     namespace: str,
 ):
+    annotated_chunks, vectors = get_vectors(
+        annotated_chunks_filepath, embedding_function
+    )
     ns = tpuf.Namespace(namespace)
-    annotated_chunks = pd.read_json(annotated_chunks_filepath, lines=True)
-
-    vectors = embedding_function(annotated_chunks["text"].tolist())
     logger.info(f"Writing {len(vectors)} vectors to Turbopuffer")
     ids = [
         f"{row['doc_name']}_{row['chunk_index']}"
         for row in annotated_chunks.itertuples()
     ]
-
     # Take all the columns from annotated_chunks and add them to the upsert_columns
     columns = annotated_chunks.to_dict(orient="records")
     ns.write(
