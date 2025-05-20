2544f5772261404a0f924306b761b25ba7ded373
NirantK <nirant.bits@gmail.com>
2025-05-21 04:32:56 +0530

    feat(cli): add evaluate command for RAG workflow with data processing and scoring

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import os
+from pathlib import Path
 
 import click
+import pandas as pd
 from dotenv import load_dotenv
 from loguru import logger
+from tqdm.auto import tqdm
 
 from src.agentic_rag import rag_dag
+from src.evaluate import evaluate_rag
 from src.ingest import to_turbopuffer
 from src.search_tools import hybrid_search
 from src.simple_rag import rag_chain
 
 load_dotenv()
+
+tqdm.pandas()
 
 
 @click.group()
@@ -56,6 +62,37 @@ def agentic_rag(query):
     """Run a simple RAG workflow"""
     response = rag_dag(query)
     logger.info(response)
+
+
+@cli.command()
+@click.option(
+    "--data-filepath", default="data/ground_truth/financebench_open_source.jsonl", help="Path to ground truth data"
+)
+@click.option("--output-datapath", default="data/evals/", help="Path to output data")
+@click.option("--eval-setup", default="simple", help="Evaluation setup")
+def evaluate(data_filepath, output_datapath, eval_setup):
+    """Evaluate the RAG workflow"""
+    df = pd.read_json(data_filepath, lines=True)
+
+    # df = df.head(10)
+    def process_query(row):
+        query = row["question"]
+        if eval_setup == "simple":
+            generated_answer = rag_chain(query)
+        elif eval_setup == "agentic":
+            generated_answer = rag_dag(query)
+        else:
+            raise ValueError(f"Invalid evaluation setup: {eval_setup}")
+        ground_truth = row["answer"]
+        score, reasoning = evaluate_rag(query, generated_answer, ground_truth)
+        return pd.Series([generated_answer, score, reasoning])
+
+    results = df.progress_apply(process_query, axis=1)
+    results.columns = ["generated_answer", "score", "judge_reasoning"]
+    results = pd.concat([df, results], axis=1)
+    output_datapath = Path(output_datapath) / f"{eval_setup}_results.jsonl"
+    output_datapath.parent.mkdir(parents=True, exist_ok=True)
+    results.to_json(output_datapath, orient="records", lines=True)
 
 
 def main():
